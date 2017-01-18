f0f98414648b1be9fde266abe767a4e3188a5d02
REvanCof <rogerecoffey@gmail.com>
2017-01-18 13:47:57 -0600

    Everything set up

---
+++ assets/javascript/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'GusDqn1I5PZ9TKmYE2Bfxmox0',
+  consumer_secret: 'jzdXQ6vY7R6i4xWVLwAXzmeE1C5npX4BiCtdaLA39XYSoGrX8J',
+  access_token_key: '545365851-iaWFYEHr1yoH1MvyGMX8Ztj53Kc3T71Fwb0eZNjL',
+  access_token_secret: '3yCgm7hpnPRhHSEzzcqOHfUsBZItPWkmGHOSwJhxXLNL8',
+}
